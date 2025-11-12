cc8089dddb4b0f8b2346e9ec6a1b22dee9775381
Aayusha62 <adhikaryaayusa@gmail.com>
2025-11-12 08:36:13 +0545

    new branch

---
+++ week3/question1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, sum = 0;
+
+    cout << "Enter a positive number: ";
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        sum = sum + i;
+    }
+
+    cout << "The sum of first " << N << " natural numbers is: " << sum << endl;
+
+    return 0;
+}
